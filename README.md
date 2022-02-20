e76142a885bfaa2da0dbea84786a1c69fa9b1183
gervanna <stephenslavern@gmail.com>
2022-02-19 17:43:27 -0700

    RPS game with UI

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # odin-rps
-Project: Rock Paper Scissors
+Project: Rock Paper Scissors (Lizard Spock)
+
+The initial project was to make the RPS game and have it playable in the console for five rounds. This version has a GUI and used the DOM methods to achieve this. 
+
+See the Live code here: 
+
+
--- script.js
+++ script.js
@@ -1,57 +1,130 @@
+//GAME LOGIC
 let playerScore = 0;
 let computerScore = 0;
-let gameRounds = 0;
+let roundResults;
 
-//generates 3 random #'s for the rock/paper/scissors choices
 function computerPlay() {
-    let randomChoice = Math.floor(Math.random()*3);
-    switch (randomChoice) { //used switch statement to assign #'s to the appropriate strings
+    let randomChoice = Math.floor(Math.random()*5);
+    switch(randomChoice) {
         case 0:
-            return 'rock';
+            return 'ROCK';
         case 1:
-            return 'paper';
+            return 'PAPER';
         case 2:
-            return 'scissors';
+            return 'SCISSORS';
+        case 3:
+            return 'LIZARD';
+        case 4:
+            return 'SPOCK';
     }
 }
-
-function playRound(playerSelection, computerSelection) { //func with game logic to check winner/loser
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
-        return "It's a tie.";
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        playerScore++;
-        return "You win. Rock smashes scissors.";
-    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        computerScore++;
-        return "You lose. Rock smashes scissors.";
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        return "You win. Paper covers rock.";
-    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        computerScore++;
-        return "You lose. Paper covers rock.";
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++;
-        return "You win. Scissors cuts paper.";
-    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        computerScore++;
-        return "You lose. Scissors cuts paper.";
+        roundResults = [playerSelection, computerSelection, 'tie'];
     }
+    if (
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
+        (playerSelection === 'ROCK' && computerSelection === 'LIZARD') ||
+        (playerSelection === 'LIZARD' && computerSelection === 'SPOCK') ||
+        (playerSelection === 'SPOCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'LIZARD') ||
+        (playerSelection === 'LIZARD' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'SPOCK') ||
+        (playerSelection === 'SPOCK' && computerSelection === 'ROCK') ||
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS')
+      ) {
+        playerScore++;
+        roundResults = [playerSelection, computerSelection, 'You'];
+    }
+    if (
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK') ||
+        (computerSelection === 'ROCK' && playerSelection === 'LIZARD') ||
+        (computerSelection === 'LIZARD' && playerSelection === 'SPOCK') ||
+        (computerSelection === 'SPOCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'LIZARD') ||
+        (computerSelection === 'LIZARD' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'SPOCK') ||
+        (computerSelection === 'SPOCK' && playerSelection === 'ROCK') ||
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS')
+      ) {
+        computerScore++;
+        roundResults = [playerSelection, computerSelection, 'The computer'];
+    }
+    return roundResults;
 }
 
-function runGame() {
-    while (gameRounds < 5) { //while inside runGame in order to play 5 rounds and get a new choice each round
-        const playerSelection = window.prompt("Type: rock, paper or scissors ").toLowerCase(); //promt for user choice and toLowerCase makes string lower before running checks
-        const computerSelection = computerPlay(); //computer choice from random # func
-        console.log(playRound(playerSelection, computerSelection)); //rounds of the game
-        gameRounds++ //increments gameRounds in order to end the loop after 5 rounds
-    }
-    if (playerScore === computerScore) {
-        return "\nGAME OVER! The game is tied.";
-    } else if (playerScore > computerScore) {
-        return "\nGAME OVER! You won the game.";
-    } else {
-        return "\nGAME OVER! The computer won the game.";
+//TO ALLOW FOR DOM MANIPULATION TO ACCESS UI?
+const scoreDetail = document.getElementById('scoreDetail');
+const scoreMsgs = document.getElementById('scoreMsgs');
+const playerChoice = document.getElementById('playerChoice');
+const computerChoice = document.getElementById('computerChoice');
+const playerScoreCurrent = document.getElementById('playerScore');
+const computerScoreCurrent = document.getElementById('computerScore');
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+const lizardBtn = document.getElementById('lizardBtn');
+const spockBtn = document.getElementById('spockBtn');
+const restartBtn = document.getElementById('restartBtn');
+
+rockBtn.addEventListener('click', () => handleButtonValue('ROCK'));
+paperBtn.addEventListener('click', () => handleButtonValue('PAPER'));
+scissorsBtn.addEventListener('click', () => handleButtonValue('SCISSORS'))
+lizardBtn.addEventListener('click', () => handleButtonValue('LIZARD'))
+spockBtn.addEventListener('click', () => handleButtonValue('SPOCK'))
+
+function handleButtonValue (buttonName) {
+    result = playRound(buttonName);
+    updateScoreBoard(result);
+    isGameWon(); 
+};
+
+function isGameWon (){
+    if (playerScore === 5 || computerScore === 5) {
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+        lizardBtn.disabled = true;
+        spockBtn.disabled = true;
     }
 }
-console.log(runGame()); //logs the entire game to console. 
+restartBtn.addEventListener('click', function() {
+    playerScore = 0;
+    computerScore = 0;
+    roundResults = null;
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+    lizardBtn.disabled = false;
+    spockBtn.disabled = false;
+    scoreDetail.textContent = "What will you choose?";
+    scoreMsgs.textContent = "First to 5 points is the winner!"
+    playerScoreCurrent.textContent = 'Player: 0';
+    computerScoreCurrent.textContent = 'Computer: 0';
+    playerChoice.textContent = null;
+    computerChoice.textContent = null;
+});
+
+function updateScoreBoard (result) {
+    playerScoreCurrent.textContent = `Player: ${playerScore}`;
+    computerScoreCurrent.textContent = `Computer: ${computerScore}`;
+    let roundChoices;
+    let roundMsg;
+    if (result[2] === 'tie') {
+        roundMsg = `${result[0]} ties with ${result[1]}.`
+        roundChoices = "It's a tie!"
+    } else if (result[2] === 'You') {
+        roundMsg = `${result[0]} beats ${result[1]}.`;
+        roundChoices = "You won!"
+    } else {
+        roundMsg = `${result[1]} beats ${result[0]}.`
+        roundChoices = "You lost!"
+    }
+    scoreDetail.textContent = roundChoices;
+    scoreMsgs.textContent = roundMsg;
+    playerChoice.textContent = `${result[0]}`
+    computerChoice.textContent = `${result[1]}`
+};
